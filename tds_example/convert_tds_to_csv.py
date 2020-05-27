ae1c869868ba7aa80f2cc3ad49cf4cd7d1c5f856
Lukas Prediger <lukas.m.prediger@aalto.fi>
2020-05-27 10:32:25 +0300

    tds_example/convert_tds_to_csv.py now downloads dataset if not present

--- tds_example/convert_tds_to_csv.py
+++ tds_example/convert_tds_to_csv.py
@@ -1,18 +1,26 @@
 import pandas as pd
+import os.path
+from urllib.request import urlretrieve
 
 """
 dataset.xlsx is from https://github.com/souzatharsis/covid-19-ML-Lab-Test/blob/master/data/dataset.xlsx
 """
 
-original_data = pd.read_excel("dataset.xlsx") 
+dataset_path = "dataset.xlsx"
+if not os.path.exists(dataset_path):
+    print("dataset.xlsx not found locally, downloading....")
+    dataset_url = "https://github.com/souzatharsis/covid-19-ML-Lab-Test/raw/master/data/dataset.xlsx"
+    urlretrieve(dataset_url, dataset_path)
+
+original_data = pd.read_excel("dataset.xlsx")
 feature_names = list(original_data.columns)
 feature_names = [name.strip() for name in feature_names]
 tds_data = pd.DataFrame(original_data.values, columns=feature_names)
 
 ## save top 10 features
-top10_features_handle = open("tds_top10_features.txt", "r")
-top10_features = top10_features_handle.readlines()
-top10_features_handle.close()
+top10_features = []
+with open("tds_top10_features.txt", "r") as top10_features_handle:
+    top10_features = top10_features_handle.readlines()
 
 outcome_var = "SARS-Cov-2 exam result"
 top10_features.append(outcome_var)
@@ -24,9 +32,9 @@ tds_data.to_csv("tds_data.csv", index=False)
 tds_top10.to_csv("tds_top10.csv", index=False)
 
 ### save also  data set with all features used in TDS example
-all_features_handle = open("tds_all_features.txt", "r")
-all_features = all_features_handle.readlines()
-all_features_handle.close()
+all_features = []
+with open("tds_all_features.txt", "r") as all_features_handle:
+    all_features = all_features_handle.readlines()
 
 outcome_var = "SARS-Cov-2 exam result"
 all_features.append(outcome_var)
