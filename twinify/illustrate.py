21c818358b0b764585aa2581e7353f0939d29ff9
Lukas Prediger <lukas.m.prediger@aalto.fi>
2020-06-23 11:49:35 +0300

    fixing and nicefying visualizations

--- twinify/illustrate.py
+++ twinify/illustrate.py
@@ -2,6 +2,8 @@ import numpy as onp
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+from mpl_toolkits.axes_grid1 import ImageGrid
+from collections import OrderedDict as od
 
 syn_data_color = 'red'
 syn_data_label = 'Synthetic data'
@@ -14,14 +16,16 @@ def plot_margins(syn_df, orig_df, show=False):
     blue_patch = mpatches.Patch(color=orig_data_color, label=orig_data_label)
     axis.legend(handles=[blue_patch, red_patch])
 
-    syn_violin_parts = axis.violinplot(syn_df.dropna().values, onp.arange(0, syn_df.shape[1]))
-    for syn_violin_part in syn_violin_parts['bodies']:
-        syn_violin_part.set_color(syn_data_color)
-    syn_violin_parts["cmaxes"].set_color(syn_data_color)
-    syn_violin_parts["cmins"].set_color(syn_data_color)
-    syn_violin_parts["cbars"].set_color(syn_data_color)
+    for location, name in enumerate(syn_df.columns):
+        col = syn_df[name].dropna().values
+        if col.size == 0:
+            continue
+        syn_violin_part = axis.violinplot(col, [location])
+        syn_violin_part['bodies'][0].set_color(syn_data_color)
+        syn_violin_part["cmaxes"].set_color(syn_data_color)
+        syn_violin_part["cmins"].set_color(syn_data_color)
+        syn_violin_part["cbars"].set_color(syn_data_color)
 
-    from collections import OrderedDict as od
     orig_violin_parts = od()
     for location, name in enumerate(syn_df.columns):
         orig_violin_parts[name] = axis.violinplot(orig_df[name].dropna().values, [location])
@@ -33,32 +37,35 @@ def plot_margins(syn_df, orig_df, show=False):
         orig_violin_part["cbars"].set_color(orig_data_color)
 
     axis.set_xticks(onp.arange(0, syn_df.shape[1]))
-    axis.set_xticklabels(list(syn_df.columns))
+    axis.set_xticklabels(list(syn_df.columns), rotation=45)
     if show:
         fig.show()
     return fig
 
 def plot_covariance_heatmap(syn_df, orig_df, show=False):
-    syn_cov = syn_df.dropna().cov().values
-    orig_cov = orig_df.dropna().cov().values
-    fig, axis = plt.subplots(nrows=1, ncols=2)
-    cov_max = onp.max([orig_cov, syn_cov])
-    cov_min = onp.min([orig_cov, syn_cov])
-    axis[0].imshow(orig_cov, vmin=cov_min, vmax=cov_max)
-    axis[0].set_xticks(onp.arange(0, syn_df.shape[1]))
-    axis[0].set_yticks([])
-    axis[0].set_xticklabels(list(syn_df.columns), rotation="vertical", fontdict={"fontsize":10})
-    axis[0].set_title(orig_data_label)
+    syn_cov = syn_df.cov().values
+    orig_cov = orig_df.cov().values
+    cov_max = onp.maximum(onp.max(orig_cov[onp.isfinite(orig_cov)]), onp.max(syn_cov[onp.isfinite(syn_cov)]))
+    cov_min = onp.minimum(onp.min(orig_cov[onp.isfinite(orig_cov)]), onp.min(syn_cov[onp.isfinite(syn_cov)]))
 
-    im = axis[1].imshow(syn_cov, vmin=cov_min, vmax=cov_max)
-    axis[1].set_xticks(onp.arange(0, syn_df.shape[1]))
-    axis[1].set_yticks([])
-    axis[1].set_xticklabels(list(syn_df.columns), rotation="vertical", fontdict={"fontsize":10})
-    axis[1].set_title(syn_data_label)
+    fig = plt.figure()
+    grid = ImageGrid(fig, 111, nrows_ncols=(1,2), axes_pad=0.15, share_all=True, cbar_location="right", cbar_mode="single", cbar_pad=0.15)
 
-    fig.subplots_adjust(right=0.8)
-    cbar_ax = fig.add_axes([0.85, 0.15, 0.02, 0.7])
-    fig.colorbar(im, cax=cbar_ax)
+    grid[0].imshow(orig_cov, vmin=cov_min, vmax=cov_max)
+    grid[0].set_xticks(onp.arange(0, syn_df.shape[1]))
+    grid[0].set_yticks([])
+    grid[0].set_xticklabels(list(syn_df.columns), rotation=45, fontdict={"fontsize":10})
+    grid[0].set_title(orig_data_label)
+
+    im = grid[1].imshow(syn_cov, vmin=cov_min, vmax=cov_max)
+    grid[1].set_xticks(onp.arange(0, syn_df.shape[1]))
+    grid[1].set_yticks(onp.arange(0, syn_df.shape[1]))
+    grid[1].set_xticklabels(list(syn_df.columns), rotation=45, fontdict={"fontsize":10})
+    grid[1].set_yticklabels(list(syn_df.columns), fontdict={"fontsize":10})
+    grid[1].set_title(syn_data_label)
+
+    grid[1].cax.colorbar(im)
+    grid[1].cax.toggle_label(True)
 
     if show:
         fig.show()
@@ -74,7 +81,7 @@ def plot_missing_values(syn_df, orig_df, show=False):
     axis.bar(x_ticks-.5*bar_width, syn_nas, width=bar_width, color=syn_data_color, edgecolor='white', label=syn_data_label)
     axis.bar(x_ticks+.5*bar_width, orig_nas, width=bar_width, color=orig_data_color, edgecolor='white', label=orig_data_label)
     axis.set_xticks(x_ticks)
-    axis.set_xticklabels(list(syn_nas.index))
+    axis.set_xticklabels(list(syn_nas.index), rotation=45)
 
     red_patch = mpatches.Patch(color=syn_data_color, label=syn_data_label)
     blue_patch = mpatches.Patch(color=orig_data_color, label=orig_data_label)
