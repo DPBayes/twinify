343b41c10dcc5cbbeb01fda89c1d924a02761fe8
Lukas Prediger <lukas.m.prediger@aalto.fi>
2020-06-24 11:24:08 +0300

    cleaning up old files

--- tds_example/models/create_model_txt_full.py
+++ tds_example/models/create_model_txt_full.py
@@ -5,16 +5,16 @@ data = pd.read_csv("../tds_all.csv")
 
 model_str = ""
 for column in data.columns:
-	feature = data[column]
-	if feature.dtype == 'O':
-		if len(np.unique(feature.dropna()))==2:
-			model_str += "{}: Bernoulli\n".format(column)
-		else:
-			model_str += "{}: Categorical\n".format(column)
-	elif feature.dtype == 'int':
-		model_str += "{}: Poisson\n".format(column)
-	elif feature.dtype == 'float':
-		model_str += "{}: Normal\n".format(column)
+    feature = data[column]
+    if feature.dtype == 'O':
+        if len(np.unique(feature.dropna()))==2:
+            model_str += "{}: Bernoulli\n".format(column)
+        else:
+            model_str += "{}: Categorical\n".format(column)
+    elif feature.dtype == 'int':
+        model_str += "{}: Poisson\n".format(column)
+    elif feature.dtype == 'float':
+        model_str += "{}: Normal\n".format(column)
 handle = open("full_model.txt", "w")
 handle.writelines(model_str)
 handle.close()
