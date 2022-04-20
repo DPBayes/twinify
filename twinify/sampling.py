e326afec8bdef1a418d270984331c89878c4eaf1
Lukas Prediger <lukas.m.prediger@aalto.fi>
2022-04-20 15:08:09 +0300

    Added docstrings in twinify/sampling.py

--- twinify/sampling.py
+++ twinify/sampling.py
@@ -30,8 +30,17 @@ def sample_synthetic_data(
         num_record_samples_per_parameter_sample: int
     ) -> Dict[str, jnp.ndarray]:
     """
-    Returns a dictionary jnp.arrays of synthetic data samples for each sample site in the model.
-    Each of these has shape (num_parameter_samples, num_synthetic_records_per_parameter_sample, *sample_site_shape)
+    Args:
+        model: Model function.
+        guide: Guide // variational approximation function.
+        posterior_params: Dictionary of learned parameters for the guide/model.
+        sampling_rng: JAX RNG key for sampling.
+        num_parameter_samples: How many different parameter sets to sample from the guide.
+            For each of these a synthetic dataset will be sampled.
+        num_record_samples_per_parameter_sample: The number of synthetic data points sampled for each parameter set sample.
+    Returns:
+        A dictionary of jnp.arrays of synthetic data samples for each sample site in the model.
+        Each of these has shape (num_parameter_samples, num_synthetic_records_per_parameter_sample, *sample_site_shape)
     """
 
     @jax.jit
@@ -70,8 +79,7 @@ PreparedPostprocessFunction = Callable[[Dict[str, jnp.ndarray]], pd.DataFrame]
 def reshape_and_postprocess_synthetic_data(
         posterior_samples: Dict[str, jnp.ndarray],
         prepared_postprocess_fn: PreparedPostprocessFunction,
-        separate_output: bool,
-        num_parameter_samples: int
+        separate_output: bool
     ) -> Iterable[Tuple[pd.DataFrame, pd.DataFrame]]:
     """ Given a dictionary of posterior predictive samples as output by sample_synthetic_data,
     organises it into separate-per-parameter-sample or a single large data set(s) and
@@ -85,6 +93,24 @@ def reshape_and_postprocess_synthetic_data(
     Postprocessing is performed to transform the data used internally by numpyro so
     that the synthetic twin looks like the original data, using either a user-provided
     `postprocess` function or one set-up automatically in the case of automodelling.
+
+    Args:
+        posterior_samples: A dictionary of synthetic data samples as output by`sample_synthetic_data`.
+            Must contain a single jnp.array for each sample site in the model, each of which
+            must have shape (num_parameter_samples, num_synthetic_records_per_parameter_sample, *sample_site_shape).
+        prepared_postprocess_fn: A function for postprocessing raw samples, accepting as input
+            a dictionary containing a jnp.array for each sample site in the model, each of which
+            has shape (num_data, *sample_site_shape).
+        separate_output: If `True`, this function will generate `num_parameter_samples`
+            pandas data frames, each of which holding records sampled from a single sample
+            from the parameter posterior. If `False`, the result
+            will be a sequence contain a single data frame, containing all samples.
+    Returns:
+        Yields `num_parameter_samples` tuples if `separate_output` is `True`,
+        or a single tuple if `separate_output` is `False`.
+        Each such tuple consists of a two pandas DataFrames, the first containing the
+        raw numeric synthetic data as sampled from the model, the second containing the
+        postprocessed and encoded synthetic data that looks like the original input data.
     """
 
     def _squash_sample_dims(v: jnp.array) -> jnp.array:
@@ -93,6 +119,8 @@ def reshape_and_postprocess_synthetic_data(
         new_shape = (old_shape[0] * old_shape[1], *old_shape[2:])
         reshaped_v = jnp.reshape(v, new_shape)
         return reshaped_v
+
+    num_parameter_samples = posterior_samples.values()[0].shape[0]
 
     posterior_samples_array = []
     if separate_output:
