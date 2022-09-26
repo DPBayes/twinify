7302d6ebc066892cbe0cd54b0c5cb0ad4621cfa2
Lukas Prediger <lukas.m.prediger@aalto.fi>
2022-09-26 17:09:47 +0300

    MWP DPVI implementation of twinifyv2 interface
    pending tests; reduced features compared to twinify v1 (no automodelling, no pre-/postprocessing support yet)

--- twinify/dpvi.py
+++ twinify/dpvi.py
@@ -1,31 +1,46 @@
-from msilib.schema import Binary
 import pandas as pd
 
 from typing import BinaryIO, Optional, Callable, Any, BinaryIO, Dict, Union, Iterable
+
+import os
+import pickle
+import numpy as np
+from numpy.typing import ArrayLike
+import jax
+import jax.numpy as jnp
 
 import d3p.random
 import d3p.dputil
 import numpyro.infer
 import twinify.infer
 from twinify.base import InferenceModel, InferenceResult, InvalidFileFormatException
-import jax
-import os
 import twinify.serialization
+import twinify.sampling
+
 
 ModelFunction = Callable
 GuideFunction = Callable
 
+
 class DPVIModel(InferenceModel):
 
-    def __init__(self, model: ModelFunction, guide: GuideFunction) -> None:
+    def __init__(self, model: ModelFunction, guide: Optional[GuideFunction] = None) -> None:
         super().__init__()
         self._model = model
+
+        if guide is None:
+            guide = self.create_default_guide(model)
+
         self._guide = guide
 
+    @classmethod
+    @property
+    def default_guide_class(cls):
+        return numpyro.infer.autoguide.AutoDiagonalNormal
+
     @staticmethod
-    def create_with_autoguide(model: ModelFunction) -> 'DPVIModel':
-        guide = numpyro.infer.autoguide.AutoDiagonalNormal(model)
-        return DPVIModel(model, guide)
+    def create_default_guide(model: ModelFunction) -> GuideFunction:
+        return DPVIModel.default_guide_class(model)
 
     def fit(self,
             data: pd.DataFrame,
@@ -35,6 +50,8 @@ class DPVIModel(InferenceModel):
             clipping_threshold: float,
             num_iter: int,
             q: float) -> InferenceResult:
+
+        # TODO: this currently assumes that data is fully numeric (i.e., categoricals are numbers, not labels)
 
         num_data = data.size
         batch_size = int(num_data * q)
@@ -48,36 +65,66 @@ class DPVIModel(InferenceModel):
 
 class DPVIResult(InferenceResult):
 
-    def __init__(self, model: ModelFunction, guide: GuideFunction, parameters: Dict[str, Any]) -> None:
+    def __init__(self, model: ModelFunction, guide: GuideFunction, parameters: Dict[str, ArrayLike]) -> None:
         self._model = model
         self._guide = guide
         self._params = parameters
 
-    def generate(self,
-            dataset_size: int,
-            num_datasets: Optional[int] = 1,
-            ) -> Iterable[pd.DataFrame]:
-        pass
-
     def generate_extended(self,
+            rng: d3p.random.PRNGState,
             num_data_per_parameter_sample: int,
             num_parameter_samples: int,
             single_dataframe: Optional[bool] = False) -> Union[Iterable[pd.DataFrame], pd.DataFrame]:
-        pass
+
+        jax_rng = d3p.random.convert_to_jax_rng_key(rng)
+        samples = twinify.sampling.sample_synthetic_data(
+            self._model, self._guide, self._params, jax_rng, num_parameter_samples, num_data_per_parameter_sample
+        )
+
+        def _squash_sample_dims(v: jnp.array) -> jnp.array:
+            old_shape = jnp.shape(v)
+            assert len(old_shape) >= 2
+            new_shape = (old_shape[0] * old_shape[1], *old_shape[2:])
+            reshaped_v = jnp.reshape(v, new_shape)
+            return reshaped_v
+
+        if single_dataframe:
+            samples = {k: _squash_sample_dims(v) for k, v in samples.items()}
+            return pd.DataFrame(samples)
+        else:
+            for i in range(num_parameter_samples):
+                local_samples = {k: v[i] for k, v in samples.items()}
+                yield pd.DataFrame(local_samples)
+
+        # TODO: currently performs no post-processing / remapping of integer values to categoricals
 
     @classmethod
-    def _load_from_io(cls, read_io: BinaryIO) -> 'InferenceResult':
-        raise NotImplementedError("need to figure out how to reconstruct or load treedef here")
-        treedef = None
-        return DPVIResultIO.load_from_io(read_io, treedef)
+    def _load_from_io(cls, read_io: BinaryIO, model: ModelFunction, guide: Optional[GuideFunction] = None, **kwargs) -> 'InferenceResult':
+        if guide is None:
+            guide = DPVIModel.create_default_guide(model)
+
+        parameters = DPVIResultIO.load_params_from_io(read_io)
+
+        return DPVIResult(model, guide, parameters)
 
     @classmethod
     def _is_file_stored_result_from_io(cls, read_io: BinaryIO) -> bool:
         return DPVIResultIO.is_file_stored_result_from_io(read_io, reset_cursor=True)
 
     def _store_to_io(self, write_io: BinaryIO) -> None:
-        return DPVIResultIO.store_to_io(write_io, self._params)
+        return DPVIResultIO.store_params_to_io(write_io, self._params)
 
+    @property
+    def guide(self) -> GuideFunction:
+        return self._guide
+
+    @property
+    def model(self) -> ModelFunction:
+        return self._model
+
+    @property
+    def parameters(self) -> Dict[str, ArrayLike]:
+        return jax.tree_map(lambda x: np.copy(x), self._params)
 
 class DPVIResultIO:
 
@@ -86,7 +133,8 @@ class DPVIResultIO:
     CURRENT_IO_VERSION_BYTES = CURRENT_IO_VERSION.to_bytes(1, twinify.serialization.ENDIANESS)
 
     @staticmethod
-    def load_from_io(read_io: BinaryIO, treedef: jax.tree_util.PyTreeDef) -> DPVIResult:
+    # def load_from_io(read_io: BinaryIO, treedef: jax.tree_util.PyTreeDef) -> DPVIResult:
+    def load_params_from_io(read_io: BinaryIO) -> Dict[str, ArrayLike]:
         assert read_io.readable()
 
         if not DPVIResultIO.is_file_stored_result_from_io(read_io, reset_cursor=False):
@@ -96,9 +144,9 @@ class DPVIResultIO:
         if current_version != DPVIResultIO.CURRENT_IO_VERSION:
             raise InvalidFileFormatException(DPVIResult, "Stored data uses an unknown storage format version.")
 
-        parameters = twinify.serialization.read_params(read_io, treedef)
-
-        raise NotImplementedError("need to figure out how to get model and guide here")
+        # parameters = twinify.serialization.read_params(read_io, treedef)
+        # raise NotImplementedError("need to figure out how to get model and guide here")
+        return pickle.load(read_io)
 
     @staticmethod
     def is_file_stored_result_from_io(read_io: BinaryIO, reset_cursor: bool) -> bool:
@@ -115,9 +163,10 @@ class DPVIResultIO:
         return False
 
     @staticmethod
-    def store_to_io(write_io: BinaryIO, params: Any) -> None:
+    def store_params_to_io(write_io: BinaryIO, params: Dict[str, ArrayLike]) -> None:
         assert write_io.writable()
 
         write_io.write(DPVIResultIO.IDENTIFIER)
         write_io.write(DPVIResultIO.CURRENT_IO_VERSION_BYTES)
-        twinify.serialization.write_params(params, write_io)
+        pickle.dump(params, write_io)
+        # twinify.serialization.write_params(params, write_io)
