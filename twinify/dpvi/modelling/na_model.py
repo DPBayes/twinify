005219055bfff2e6c165ef4ed812448e0367cb37
Lukas Prediger <lukas.m.prediger@aalto.fi>
2022-11-08 17:10:37 +0200

    Cleanup and tests for NAModel

--- twinify/dpvi/modelling/na_model.py
+++ twinify/dpvi/modelling/na_model.py
@@ -16,38 +16,50 @@
 Missing value model used by twinify main script and available for modelling.
 """
 
-import jax.numpy as np
+import jax.numpy as jnp
+import numpy as np
 import jax
 import typing
 
-import numpyro.distributions as dist
+import numpyro.distributions as dists
 
 
-class _NAConstraint(dist.constraints.Constraint):
+class _NAConstraint(dists.constraints.Constraint):
     """ Wraps a constraint to additionally allow NaN values.
     """
 
-    def __init__(self, base_constraint: dist.constraints.Constraint) -> None:
+    def __init__(self, base_constraint: dists.constraints.Constraint) -> None:
         self.base_constraint = base_constraint
         super().__init__()
 
-    def __call__(self, value: np.ndarray) -> np.ndarray:
-        return np.isnan(value) | self.base_constraint(value)
+    def __call__(self, value: jnp.ndarray) -> jnp.ndarray:
+        return jnp.isnan(value) | self.base_constraint(value)
 
-    def feasible_like(self, prototype: np.ndarray) -> np.ndarray:
+    def feasible_like(self, prototype: jnp.ndarray) -> jnp.ndarray:
         return self.base_constraint.feasible_like(prototype)
+
+    @property
+    def is_discrete(self) -> bool:
+        return self.base_constraint.is_discrete
+
+    @property
+    def event_dim(self) -> int:
+        return self.base_constraint.event_dim
+
 
 na_constraint = _NAConstraint
 
 
-class NAModel(dist.Distribution):
+class NAModel(dists.Distribution):
     """ Model decorator for missing values.
 
     Can be used to enhance any single feature distribution to handle missing
     values (represented by nan values (`np.nan`)) in a missing completely at random fashion.
     """
 
-    def __init__(self, base_dist, na_prob=0.5, validate_args=None):
+    arg_constraints = {'na_prob': dists.constraints.unit_interval}
+
+    def __init__(self, base_dist, na_prob=0.5, validate_args=None) -> None:
         """
         Initialize a new NAModel instance.
 
@@ -59,17 +71,23 @@ class NAModel(dist.Distribution):
         self._na_prob = na_prob
         super(NAModel, self).__init__(base_dist.batch_shape, base_dist.event_shape, validate_args)
 
-
     @property
-    def support(self) -> dist.constraints.Constraint:
-        return na_constraint(self._base_dist.support)
-
-    @property
-    def base_distribution(self):
+    def base_distribution(self) -> dists.Distribution:
         """ The feature base distribution for existing data. """
         return self._base_dist
 
-    def log_prob(self, value):
+    @property
+    def support(self) -> dists.constraints.Constraint:
+        return na_constraint(self._base_dist.support)
+
+    @property
+    def has_enumerate_support(self) -> bool:
+        return self._base_dist.has_enumerate_support
+
+    def enumerate_support(self, expand=True):
+        return self._base_dist.enumerate_support(expand)
+
+    def log_prob(self, value: jnp.ndarray) -> jnp.ndarray:
         """
         Evaluates the log-probabilities for given data realizations (observed or missing)
 
@@ -78,10 +96,17 @@ class NAModel(dist.Distribution):
         Returns:
             array_like containing probabilities for each given data point
         """
-        log_probs = dist.Bernoulli(probs = self._na_prob).log_prob(np.isnan(value))
-        return log_probs + np.isfinite(value)*self._base_dist.log_prob(np.nan_to_num(value))
+        if len(self.event_shape) > 0:
+            reshaped_value = value.reshape(*(value.shape[:-len(self.event_shape)]), -1)
+            nans = jnp.isnan(reshaped_value).sum(-1) == np.prod(self.event_shape)
+        else:
+            nans = jnp.isnan(value)
+        nan_log_probs = dists.Bernoulli(probs = self._na_prob).log_prob(nans)
 
-    def sample(self, key, sample_shape=()):
+        value_log_probs = self._base_dist.log_prob(value)
+        return nan_log_probs + jnp.where(nans, 0., value_log_probs)
+
+    def sample(self, key :jax.random.KeyArray, sample_shape: typing.Tuple[int] = ()) -> jnp.ndarray:
         """
         Samples from the missing values model.
 
@@ -93,34 +118,25 @@ class NAModel(dist.Distribution):
         """
         return self.sample_with_intermediates(key, sample_shape)[0]
 
-    def sample_with_intermediates(self, key, sample_shape=()):
-        assert(len(sample_shape) <= 1)
+    def sample_with_intermediates(
+            self,
+            key :jax.random.KeyArray,
+            sample_shape: typing.Tuple[int] = ()
+        ) -> typing.Tuple[jnp.ndarray, typing.Tuple[typing.Any]]:
 
-        num_samples = 1
-        if len(sample_shape) > 0:
-            num_samples = sample_shape[0]
+        nan_rng, values_rng = jax.random.split(key)
+        nan_mask = dists.Bernoulli(probs = self._na_prob).sample(
+            nan_rng, sample_shape=(sample_shape + self.batch_shape)
+        )
 
-        keys = jax.random.split(key, num_samples)
+        values = self.base_distribution.sample(values_rng, sample_shape=sample_shape)
 
-        @jax.vmap
-        def sample_single(single_key):
-            vals_rng_key, probs_rng_key = jax.random.split(single_key, 2)
-            z = dist.Bernoulli(probs = self._na_prob).sample(probs_rng_key).astype("int")
-            vals = self._base_dist.sample(vals_rng_key)
-            orig_shape = vals.shape
-            if len(vals.shape) == 0:
-                vals = vals.reshape((1,))
-            vals = np.stack([vals, np.nan*np.ones_like(vals)])
-            assert(len(vals.shape) >= 2)
-            vals = vals[z, np.arange(vals.shape[1])]
-            assert(len(vals.shape) == len(orig_shape) or (len(vals.shape) == len(orig_shape) + 1 and vals.shape[0] == 1))
-            vals = vals.reshape(orig_shape)
-            return vals
+        # expand trailing dims for nan_mask to match event_shape
+        nan_mask = jnp.expand_dims(nan_mask, range(len(nan_mask.shape) - len(values.shape), 0))
 
-        vals = sample_single(keys)
-        if len(sample_shape) == 0:
-            vals = vals.squeeze(0)
-        return vals, ()
+        nans = jnp.where(nan_mask, jnp.nan, 0.)
+
+        return values + nans, ()
 
     def __repr__(self) -> str:
         return f"NAModel({repr(self.base_distribution)}"
